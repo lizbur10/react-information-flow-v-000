37cd26b758a666e7b8fd4b3e76e180444a1105ec
lizbur10 <liz.burton147@gmail.com>
2018-07-22 16:55:31 -0400

    Done.

--- src/Tier1.js
+++ src/Tier1.js
@@ -14,13 +14,31 @@ export default class Tier1 extends Component {
     }
   }
 
+  handleClick = () => {
+    const randomColor = getRandomColor();
+    this.setState({
+      color: randomColor,
+      childColor: getReducedColor(randomColor)
+    }) 
+  }
+
+  handleChildClick = (event) => {
+    debugger;
+    event.stopPropagation();
+    const randomColor = getRandomColor();
+    this.setState({
+      color: randomColor,
+      childColor: getReducedColor(randomColor)
+    })
+  }
+
   render() {
     // hard coded color values have been added below, though they won't be
     // present in our solution. What should they be replaced with?
     return (
-      <div onClick={() => {this.setState({color: "#000"})}} className="tier1" style={{backgroundColor: this.state.color, color: this.state.color}}>
-        <Tier2 color={"#0F0"} />
-        <Tier2 color={"#0FF"} />
+      <div onClick={this.handleClick} className="tier1" style={{backgroundColor: this.state.color, color: this.state.color}}>
+        <Tier2 handleClick={this.handleClick} handleChildClick={this.handleChildClick} color={this.state.childColor} />
+        <Tier2 handleClick={this.handleClick} handleChildClick={this.handleChildClick} color={this.state.childColor} />
       </div>
     )
   }
--- src/Tier2.js
+++ src/Tier2.js
@@ -12,13 +12,17 @@ export default class Tier2 extends Component {
     }
   }
 
+  componentWillReceiveProps(nextProps) {
+    this.setState({ childColor: getReducedColor(nextProps.color)  })
+  }
+
   render() {
     // hard coded color values have been added below, though they won't be
     // present in our solution. What should they be replaced with?
     return (
-      <div className="tier2" style={{backgroundColor: this.props.color, color: this.props.color}}>
-        <Tier3 color={"#0F0"} />
-        <Tier3 color={"#F00"} />
+      <div className="tier2" onClick={this.props.handleChildClick.bind(this)} style={{backgroundColor: this.props.color, color: this.props.color}}>
+        <Tier3 handleChildClick={this.props.handleChildClick} color={this.state.childColor} />
+        <Tier3 handleChildClick={this.props.handleChildClick} color={this.state.childColor} />
       </div>
     )
   }
